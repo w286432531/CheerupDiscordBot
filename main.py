d7499d871d4b5a0edd4c7a6c220209219a17cc0d
Jay Huang <53930607+w286432531@users.noreply.github.com>
2021-08-09 18:17:26 +0000

    adding first response

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+token=os.environ["token"]
+import discord
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("We have logged in as {0.user}.format(client)")
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith("-L"):
+    await message.channel.send("I love you.")
+
+client.run(token)
